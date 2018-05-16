9453dcdfa6388e01efcf31fd0adc32cbe20ce3d2
zhoulei <403907871@qq.com>
2018-05-16 18:09:54 +0800

    - iconfont - 简化路径 styles - stylus 设置环境变量

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
+    <home-header></home-header>
     hello world
+
   </div>
 </template>
 
 <script>
+  import Header from './components/Header'
+
   export default {
-    name: "Home"
+    name: "Home",
+    components: {
+      HomeHeader: Header,
+    }
   }
 </script>
 
