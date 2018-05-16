08a8e08eea13ffa6a41121188272abed5222a7c2
zhoulei <403907871@qq.com>
2018-05-16 17:12:55 +0800

    - 项目初始（border.css reset.css fastclick）

--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'  // 通用设置 css  不同手机初始化样式不一样
+import './assets/styles/border.css' // 通用设置 css  不同手机像素点不一样
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+fastClick.attach(document.body);   // 有的手机点击有300毫秒延迟  使用此模块可以让手机 马上点击事件触发
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
