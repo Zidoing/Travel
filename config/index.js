0251cbfa2a7a361fc6c499e8118c533d45d022ec
zhoulei <403907871@qq.com>
2018-05-23 15:09:31 +0800

    finish city css

--- config/index.js
+++ config/index.js
@@ -13,7 +13,8 @@ module.exports = {
     // 代理请求
     proxyTable: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://47.94.173.177:10082',
+        // target: 'http://localhost:8080',
         pathRewrite: {
           '^/api/': '/static/mock/'
         }
@@ -22,7 +23,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 10082, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: "CityAlphabet"
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl'
+  .list
+    display: flex
+    flex-direction column
+    justify-content center
+    position: absolute
+    right 0
+    top 1.58rem
+    bottom 0
+    width: .4rem
+    .item
+      line-height .44rem
+      text-align center
+      color $bgColor
+
+</style>
